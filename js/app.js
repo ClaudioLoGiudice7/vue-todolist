5bfc8a2b01eac625becaca6fc4176354c7845509
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-02-13 16:39:12 +0100

    entering bonus

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-// ** Bonus:**
-// 1 - oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
-// 2 - cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
-
-// Ogni todo sarà un oggetto, formato da due proprietà:
-// - text, una stringa che indica il testo del todo
-// - done, un booleano (true / false) che indica se il todo è stato fatto oppure no
-
 console.log("Vue ok", Vue);
 
 const app = Vue.createApp({
@@ -36,11 +28,12 @@ const app = Vue.createApp({
                 text: this.newTodo,
                 done: false,
             })
+        },
+
+        toggleTodo(index) {
+            this.todos[index].done = !this.todos[index].done;
         }
     }
 })
 
 app.mount("#root");
-
-// ** MILESTONE 3 **
-// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
